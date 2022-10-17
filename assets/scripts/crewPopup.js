e5742c67a4a71451858c59628bfc4c2663cab3f3
Ana Clara <anaclaranoronha.m@gmail.com>
2022-10-17 16:15:38 -0300

    popup setup

--- assets/scripts/crewPopup.js
+++ assets/scripts/crewPopup.js
@@ -2,26 +2,34 @@ function getID(id) {
     const crewData = fetch(`https://raw.githubusercontent.com/acnormun/star-trek/main/assets/data/stdata.json`)
         .then(response => response.json())
         .then(data => {
-
             const crewId = document.getElementById('popup')
             crewId.innerHTML = data.tripulation.map(crew => {
-
                 if (crew.id == id) {
-                    return `
-                        <div class="popup">
-                            <div class="popup-top">
+                    const popup = document.getElementById('popup')
+                    popup.classList.remove('hidden')
+                    return `<div class="top">
                                 <div>${crew.name}</div>
-                                <button class="close-button">&times;</button>
+                                <button  id="closePopup" onClick = "closePopup()">&times;</button>
                             </div>
-                            <div class="popup-content">
-                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi magnam aliquid praesentium pariatur iusto voluptate corrupti nesciunt sunt aliquam veniam, 
-                                hic ratione illo sapiente temporibus libero, tempore nisi labore distinctio!
-                            </div>
-
-                        </div>
-                `
+                            <div class="content">
+                                <div>
+                                    <img src="${crew.photo}"</img>
+                                </div>
+                                <div>
+                                    <ul>
+                                        <li> Nome: ${crew.name} </li>
+                                        <li> Origem: ${crew.origin} </li>
+                                </div>
+                            </div>`
                 }
-            })
+            }).join('')
         }
         )
+}
+
+function closePopup(){
+
+    const popup = document.getElementById('popup')
+    popup.classList.add('hidden')
+    
 }
