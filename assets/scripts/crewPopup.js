591df42554aa9febef1731fd080527f3c8fc3c13
Ana Clara <anaclaranoronha.m@gmail.com>
2022-10-17 15:29:19 -0300

    popup update

--- assets/scripts/crewPopup.js
+++ assets/scripts/crewPopup.js
@@ -1,18 +1,27 @@
-function getID(id){
+function getID(id) {
     const crewData = fetch(`https://raw.githubusercontent.com/acnormun/star-trek/main/assets/data/stdata.json`)
-    .then(response => response.json())
-    .then(data => {
+        .then(response => response.json())
+        .then(data => {
 
-        const crewId = document.getElementById('popup')
-        crewId.innerHTML = data.tripulation.map(crew => {
+            const crewId = document.getElementById('popup')
+            crewId.innerHTML = data.tripulation.map(crew => {
 
-            if (crew.id ==id) {
-                return `
-                <div class ="popup"> alooooo</div>
-                
-                `   
-            }
-        })  
-    }
-    )
+                if (crew.id == id) {
+                    return `
+                        <div class="popup">
+                            <div class="popup-top">
+                                <div>${crew.name}</div>
+                                <button class="close-button">&times;</button>
+                            </div>
+                            <div class="popup-content">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi magnam aliquid praesentium pariatur iusto voluptate corrupti nesciunt sunt aliquam veniam, 
+                                hic ratione illo sapiente temporibus libero, tempore nisi labore distinctio!
+                            </div>
+
+                        </div>
+                `
+                }
+            })
+        }
+        )
 }
